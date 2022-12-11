598413d4968c0934a9d2a293d382eb6ea67d9139
SURAJKP <surajkpatil444@gmail.com>
2022-12-11 10:30:41 +0530

    Add count

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+// component
+import Languages from "./components/Languages";
+function App () {
+    let topLang = [ "JavaScript", "Python", "Rust", "TypeScript", "C++", "C" ]
+    const [ languages, setLanaguages ] = useState([]);
+    const [ count, setCount ] = useState(0);
+    const dummyAPICall = () => setTimeout(() => setLanaguages(topLang), 5000)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        dummyAPICall();
+    }, []);
+    return (
+        <div className='App'>
+            <header className='App-header'>
+                {languages.length === 0 && <h2>Welcome!</h2>}
+                {languages.length ? (
+                    <Languages languages={languages} />
+                ) : (
+                    <div className='text-warning text-center m-4'>
+                        Loading languages...
+                    </div>
+                )}
+                <hr />
+                <h4>{count}</h4>
+                <button
+                    className='mb-3 btn btn-primary'
+                    onClick={() => setCount((count) => count + 1)}
+                >
+                    Add count
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default App;
